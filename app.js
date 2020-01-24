28fb94acf8a393a6278b913ed5e642bd04d8fadb
Yash Wason <yashwason@gmail.com>
2020-01-24 23:36:10 +0530

    implemented quilljs rich-text-editor + quill-delta-to-html

---
+++ app.js
@@ -0,0 +1,14 @@
+// Environment variables
+require(`dotenv`).config({path: `process.env`});
+
+const express = require(`express`),
+    app = express();
+
+app.set(`view engine`, `ejs`);
+app.use(express.static(__dirname + `/public`));
+
+app.get(`/`, (req, res) => {
+    res.render(`index`);
+})
+
+app.listen(process.env.PORT, () => { console.log(`Server listening on ${process.env.PORT}`); });
---
+++ src/main.js
@@ -0,0 +1,25 @@
+import Quill from 'quill'
+import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
+
+const quill = new Quill(`#editor`, {
+    theme: `snow`,
+    debug: `error`,
+    placeholder: `Enter content`,
+    formats: [`bold`, `italic`, `link`, `underline`],
+    modules: {
+        toolbar: [
+            [`bold`, `italic`, `underline`],
+            [`link`]
+        ]
+    }
+});
+
+const outputDiv = document.querySelector(`#output`);
+quill.on(`text-change`, (delta, oldDelta, source) => {
+    let html = new QuillDeltaToHtmlConverter(quill.getContents().ops, {
+        inlineStyles: true,
+        multiLineParagraph: false
+    }).convert();
+
+    outputDiv.innerHTML = html;
+});
